f116f5dd3f334a7722d30ce4d4cdd2e98bed8a3b
Miran <mirankavinda12@gmail.com>
2023-09-25 22:02:56 +0530

    study time user

--- pomodoro.py
+++ pomodoro.py
@@ -20,3 +20,17 @@ def start_pomodoro(work_time, break_time):
             print("Pomodoro completed!")
         else:
             print(f"Pomodoro {num_pomodoros} completed. Starting the next one!")
+
+def get_study_time():
+    while True:
+        try:
+            study_hours = float(input("Enter the total study time (hours): "))
+            return study_hours * 60  # Convert hours to minutes
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+if __name__ == "__main__":
+    os.system("clear" if os.name == "posix" else "cls")  # Clear the console screen
+    study_time = get_study_time()
+    start_pomodoro(study_time, 5)
+    print("Study session completed!")
